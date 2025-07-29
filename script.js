1bf1be8c943f85615ff940b821281e5fa5abcb58
Nunesalamino <nunesalamino@gmail.com>
2025-07-29 20:29:00 -0300

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -13,4 +13,22 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     }
+    // Inicializa o Swiper
+     new Swiper('.swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        slidesPerView: 1,
+        spaceBetween: 24,
+        autoplay: {
+            delay: 3000, 
+            disableOnInteraction: false 
+        }
+    });
 });
